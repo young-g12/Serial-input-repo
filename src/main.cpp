40f70ee8d21bec91e0ca51b5be4631b95f4b569c
G.H. Miranda <74071824+young-g12@users.noreply.github.com>
2026-01-14 15:28:34 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
 #include <Arduino.h>
 
-
+const int pin = 7;
+int count = 0;
 
 void setup() {
- Serial.begin(9600);
- Serial.println("Type 'on or off");
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Enter on or off: ");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    input.trim(); 
-
-    if (input == "on") {
-      Serial.println("LED ON");
-    } else  if (input == "off") {
-      Serial.println("LED OFF");
-    } else {
-      Serial.println("Unknown Command");
-    }
-  }
+    if (Serial.available() < 0) {
+      String input = Serial.readStringUntil('\n');
+      input.trim();
+    
+      if (input == "on") {
+        Serial.println("LED ON");
+        digitalWrite(pin, HIGH);
+      } else {
+        Serial.println("LED OFF");
+        digitalWrite(pin, LOW);
+      }
+     }
 }
+
+
